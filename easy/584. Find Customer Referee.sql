4875c188cc27a507f57411b2eaedf0e97219ff6b
Arya Mane <arya.mane03@gmail.com>
2022-07-10 03:06:00 -0400

    Update 584. Find Customer Referee.sql

--- easy/584. Find Customer Referee.sql
+++ easy/584. Find Customer Referee.sql
@@ -1,6 +1,16 @@
 /* Write your T-SQL query statement below */
 
 
-select name
-from Customer
-where coalesce(referee_id,0) <> 2
+-- select name
+-- from Customer
+-- where coalesce(referee_id,0) <> 2
+
+
+-- select name
+-- from Customer
+-- where id not in 
+-- (
+-- select id
+-- from Customer
+-- where referee_id = 2
+--     )
