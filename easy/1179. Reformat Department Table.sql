edfe435ce1150b15b6d77c93c5cd979fdc260994
Arya Mane <arya.mane03@gmail.com>
2022-07-10 01:18:13 -0400

    Create 1179. Reformat Department Table.sql

---
+++ easy/1179. Reformat Department Table.sql
@@ -0,0 +1,39 @@
+/* Write your T-SQL query statement below */
+
+
+-- select id, sum(case when month = 'Jan' then revenue else null end) as Jan_Revenue
+-- ,sum(case when month = 'Feb' then revenue else null end) as Feb_Revenue
+-- ,sum(case when month = 'Mar' then revenue else null end) as Mar_Revenue
+-- ,sum(case when month = 'Apr' then revenue else null end) as Apr_Revenue
+-- ,sum(case when month = 'May' then revenue else null end) as May_Revenue
+-- ,sum(case when month = 'Jun' then revenue else null end) as Jun_Revenue
+-- ,sum(case when month = 'Jul' then revenue else null end) as Jul_Revenue
+-- ,sum(case when month = 'Aug' then revenue else null end) as Aug_Revenue
+-- ,sum(case when month = 'Sep' then revenue else null end) as Sep_Revenue
+-- ,sum(case when month = 'Oct' then revenue else null end) as Oct_Revenue
+-- ,sum(case when month = 'Nov' then revenue else null end) as Nov_Revenue
+-- ,sum(case when month = 'Dec' then revenue else null end) as Dec_Revenue
+-- from Department
+-- group by id
+
+
+-- select id,
+--     Jan as Jan_Revenue,
+--     Feb as Feb_Revenue,
+--     Mar as Mar_Revenue,
+--     Apr as Apr_Revenue,
+--     May as May_Revenue,
+--     Jun as Jun_Revenue,
+--     Jul as Jul_Revenue,
+--     Aug as Aug_Revenue,
+--     Sep as Sep_Revenue,
+--     Oct as Oct_Revenue,
+--     Nov as Nov_Revenue,
+--     Dec as Dec_Revenue
+-- from Department
+-- pivot(
+
+--     sum(revenue)
+--     for month in (Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec)
+    
+-- )a
