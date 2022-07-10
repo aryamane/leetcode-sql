9b7b0b816573cef2848b1381781933ed65880141
Arya Mane <arya.mane03@gmail.com>
2022-07-10 01:29:26 -0400

    Create 596. Classes More Than 5 Students.sql

---
+++ easy/596. Classes More Than 5 Students.sql
@@ -0,0 +1,14 @@
+/* Write your T-SQL query statement below */
+
+
+-- select class
+-- from 
+-- (select class,count(student) as student_count
+-- from Courses
+-- group by class)a
+-- where student_count >= 5
+
+-- select class
+-- from courses
+-- group by class
+-- having count(student)>=5;
