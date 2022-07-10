b320e3793d1d5db0988ef72f2e0bd76c35d5a444
Arya Mane <arya.mane03@gmail.com>
2022-07-10 16:02:41 -0400

    Create 1141. User Activity for the Past 30 Days I.sql

---
+++ easy/1141. User Activity for the Past 30 Days I.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+
+
+-- select activity_date as day, count(distinct user_id) as active_users
+-- from Activity
+-- where activity_date between dateadd(day,-29,'2019-07-27') and '2019-07-27'
+-- and activity_type is not null
+-- group by activity_date
