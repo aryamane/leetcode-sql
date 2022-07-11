2b0759d4823a66513cbf4fdadbf1fdbc0a9f97ba
Arya Mane <arya.mane03@gmail.com>
2022-07-10 21:52:57 -0400

    Create 1677. Product's Worth Over Invoices.sql

---
+++ easy/1677. Product's Worth Over Invoices.sql
@@ -0,0 +1,16 @@
+/* Write your T-SQL query statement below */
+
+
+-- select name,
+-- sum(isnull(rest,0)) rest,
+-- sum(isnull(paid,0)) paid,
+-- sum(isnull(canceled,0)) canceled,
+-- sum(isnull(refunded,0)) refunded
+-- from Product p 
+-- left join Invoice i on p.product_id = i.product_id
+-- group by name
+-- order by name
+
+
+
+
